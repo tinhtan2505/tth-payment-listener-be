5b6220305ab4e1f82180e91ced28f30828f168e5
tinhtan2505 <61924615+tinhtan2505@users.noreply.github.com>
2025-08-02 13:43:06 +0700

    Initialize Spring Boot project with Gradle
    Set up a new Spring Boot application using Gradle, including basic project structure, build configuration, PostgreSQL datasource, and initial test class. Added Gradle wrapper and configuration files for cross-platform builds.

---
+++ src/main/java/tth_payment_listener_be/tth_payment_listener_be/TthPaymentListenerBeApplication.java
@@ -0,0 +1,13 @@
+package tth_payment_listener_be.tth_payment_listener_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TthPaymentListenerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TthPaymentListenerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tth_payment_listener_be/tth_payment_listener_be/TthPaymentListenerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package tth_payment_listener_be.tth_payment_listener_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TthPaymentListenerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
