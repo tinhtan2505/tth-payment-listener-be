098ede71edae7a5acb9511605c517330d1df5ceb
tinhtan2505 <61924615+tinhtan2505@users.noreply.github.com>
2025-08-07 11:41:56 +0700

    Update CORS origin and server port configuration
    Changed allowed CORS origin to http://localhost:8686 in SecurityConfig and updated server port to 8888 in application.properties to match new development environment settings.

--- src/main/java/tth_group/payment_listener/authentication/SecurityConfig.java
+++ src/main/java/tth_group/payment_listener/authentication/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Thêm miền của bạn
+        configuration.setAllowedOrigins(List.of("http://localhost:8686")); // Thêm miền của bạn
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*")); // Cho phép tất cả các header
         configuration.setAllowCredentials(true); // Nếu bạn cần hỗ trợ cookie
